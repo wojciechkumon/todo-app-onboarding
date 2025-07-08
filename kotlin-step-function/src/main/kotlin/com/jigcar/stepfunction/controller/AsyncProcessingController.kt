0e9972c3882ddd22ccb56d39a79d5f1d1c0e7d31
Wojciech Kumoń <wojciechkumon@gmail.com>
2025-07-08 13:40:08 +0200

    AsyncProcessingController tests

--- kotlin-step-function/src/main/kotlin/com/jigcar/stepfunction/controller/AsyncProcessingController.kt
+++ kotlin-step-function/src/main/kotlin/com/jigcar/stepfunction/controller/AsyncProcessingController.kt
@@ -5,15 +5,18 @@ import com.jigcar.stepfunction.model.TaskStatusData
 import com.jigcar.stepfunction.service.StepFunctionExecutionService
 import com.jigcar.stepfunction.util.isValidUuid
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Status
 
 @Controller("/async-tasks")
 class AsyncProcessingController(private val executionService: StepFunctionExecutionService) {
 
     @Post("/")
+    @Status(HttpStatus.CREATED)
     fun start(): TaskStatusData = executionService.start()
 
     @Get("/{id}")
